a883926b7c4cf36b8b5dc1cd8f9a757489258b41
rmannibucau <rmannibucau@apache.org>
2016-12-04 21:07:56 +0100

    upgrading to angular 2.3 rc0

--- src/main/frontend/src/polyfills.ts
+++ src/main/frontend/src/polyfills.ts
@@ -1,11 +1,11 @@
 import 'core-js/es6';
 import 'core-js/es7/reflect';
-require('zone.js/dist/zone');
+import 'zone.js/dist/zone';
 
 // safari
 import './app/polyfill/date';
-require('intl');
-require('intl/locale-data/jsonp/en');
+import 'intl';
+import 'intl/locale-data/jsonp/en';
 
 if (process.env.compileEnv === 'dev') {
   Error['stackTraceLimit'] = Infinity;
